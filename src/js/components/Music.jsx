93a0639f357ad9c28dbbc665c3c37ae871bf038d
dmxride <the.misterie@gmail.com>
2017-07-03 12:05:32 +0100

    music component

--- src/js/components/Music.jsx
+++ src/js/components/Music.jsx
@@ -1,6 +1,8 @@
 'use strict';
 
 import React from 'react';
+import CLAudioPlayer from 'react-cl-audio-player';
+
 import {Row, Column, Icon, Button} from 'react-foundation';
 import _ from 'lodash';
 
@@ -8,6 +10,25 @@ export default class Music extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.songs = [
+            {
+                url: '../../assets/music/Bradio/Flyers.mp3',
+                cover: '../../assets/music/Bradio/home_bg.jpg',
+                artist: {
+                    name: 'Bradio',
+                    song: 'Flyers'
+                }
+            },
+            {
+                url: '../../assets/music/Slayer/RainingBlood.mp3',
+                cover: '../../assets/music/Slayer/maxresdefault.jpg',
+                artist: {
+                    name: 'Slayer',
+                    song: 'Raining Blood'
+                }
+            }
+        ];
 
         this.state = {
         };
@@ -19,7 +40,7 @@ export default class Music extends React.Component {
     render() {
         return (
             <div className="music">
-
+                <CLAudioPlayer songs={this.songs} autoplay />
             </div>
         );
     };
--- src/js/components/MusicSmall.jsx
+++ src/js/components/MusicSmall.jsx
@@ -1,6 +1,8 @@
 'use strict';
 
 import React from 'react';
+import CLAudioPlayer from 'react-cl-audio-player';
+
 import {Row, Column, Icon, Button} from 'react-foundation';
 import _ from 'lodash';
 
@@ -9,20 +11,61 @@ export default class MusicSmall extends React.Component {
     constructor(props) {
         super(props);
 
+        this._handleKey = this._handleKey.bind(this);
+        this.songs = [
+            {
+                url: '../src/assets/music/Bradio/Flyers.mp3',
+                cover: '../src/assets/music/Bradio/home_bg.jpg',
+                artist: {
+                    name: 'Bradio',
+                    song: 'Flyers'
+                }
+            },
+            {
+                url: '../src/assets/music/Slayer/RainingBlood.mp3',
+                cover: '../src/assets/music/Slayer/maxresdefault.jpg',
+                artist: {
+                    name: 'Slayer',
+                    song: 'Raining Blood'
+                }
+            }
+        ];
+
         this.state = {
         };
     }
 
     componentWillMount(){
+        document.addEventListener('keydown', this._handleKey);
     }
 
     render() {
         return (
-            <div className="">
-                <p>Música</p>
+            <div className="music-small">
+                <CLAudioPlayer ref="music" songs={this.songs} autoplay />
             </div>
         );
     };
 
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this._handleKey);
+    };
+
+    _handleKey(event) {
+
+        if(event.keyCode==39){
+            this.refs.music.next()
+        } else if (event.keyCode==37){
+            this.refs.music.previous()
+        } else if (event.keyCode==38){
+            if (this.refs.music.audio.volume != 1){
+                this.refs.music.audio.volume += 0.2;
+            }
+        } else if (event.keyCode==40){
+            if (this.refs.music.audio.volume > 0.2){
+                this.refs.music.audio.volume -= 0.2;
+            }
+        }
+    }
 };
 
