410695400fb7abe5fb4c55593d791eb660dff14b
martin <m.vaz.mota@gmail.com>
2017-07-05 12:23:12 +0100

    file loader + cdn music

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -64,7 +64,7 @@ module.exports = {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000&mimetype=application/font-woff"
             },{
-                test: /\.(ttf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                test: /\.(ttf|otf|eot|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "file-loader"
             },{
                 test: /\.mp4$/,
