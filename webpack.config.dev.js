71d9cc6d3e255038ccc31d2d53289859563981a5
martin <m.vaz.mota@gmail.com>
2017-07-09 18:00:21 +0100

    pls work now

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -71,9 +71,8 @@ module.exports = {
                 loader: 'file-loader',
                 include: path.join(__dirname, 'src/assets/images')
             },{
-                test: /\.wav$/,
+                test: /\.(wav|mp3)$/,
                 loader: 'file-loader',
-                include: path.join(__dirname, 'src/assets/music')
             },{
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
                 loader: "url-loader?limit=10000&mimetype=application/font-woff"
